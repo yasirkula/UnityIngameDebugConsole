0a158a8e104d0d7276ae1809f40409edbf4f1a70
tonygiang <tony_s_giang@yahoo.com>
2021-04-07 18:50:18 +0700

    Add PlayerPrefs commands.

---
+++ Plugins/IngameDebugConsole/Scripts/Commands/PlayerPrefsCommand.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace IngameDebugConsole.Commands
+{
+	public class PlayerPrefsCommands
+	{
+		[ConsoleMethod("pp.get",
+			"Get the value of a PlayerPrefs field",
+			"key",
+			"type")]
+		public static string PlayerPrefsGet(string key, string type)
+		{
+			if (!PlayerPrefs.HasKey(key)) return "Key Not Found";
+			if (type.ToLower() == "string") return PlayerPrefs.GetString(key);
+			if (type.ToLower() == "float") return PlayerPrefs.GetFloat(key).ToString();
+			if (type.ToLower() == "int") return PlayerPrefs.GetInt(key).ToString();
+			return "Invalid types";
+		}
+
+		[ConsoleMethod("pp.set",
+			"Set the value of a PlayerPrefs field",
+			"key",
+			"type",
+			"value")]
+		public static void PlayerPrefsSet(string key, string type, string value)
+		{
+			if (type.ToLower() == "string") PlayerPrefs.SetString(key, value);
+			if (type.ToLower() == "float") PlayerPrefs.SetFloat(key, float.Parse(value));
+			if (type.ToLower() == "int") PlayerPrefs.SetInt(key, int.Parse(value));
+		}
+
+		[ConsoleMethod("pp.del", "Delete a PlayerPrefs field", "key")]
+		public static void PlayerPrefsDelete(string key) => PlayerPrefs.DeleteKey(key);
+
+		[ConsoleMethod("pp.clear", "Delete all PlayerPrefs fields", "key")]
+		public static void PlayerPrefsClear(string key) => PlayerPrefs.DeleteAll();
+	}
+}
