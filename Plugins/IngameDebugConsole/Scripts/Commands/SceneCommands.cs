7883c1d18ed960a6fac18c09a6baf7a0f7aaded1
tonygiang <tony_s_giang@yahoo.com>
2021-04-07 22:02:37 +0700

    Reformat new files to follow contribution guidelines.

--- Plugins/IngameDebugConsole/Scripts/Commands/SceneCommands.cs
+++ Plugins/IngameDebugConsole/Scripts/Commands/SceneCommands.cs
@@ -6,16 +6,20 @@ namespace IngameDebugConsole.Commands
 {
 	public class SceneCommands
 	{
-		[ConsoleMethod("scene.load", "Load a scene", "sceneName", "mode")]
-		public static void LoadScene(string sceneName, string mode)
+		[ConsoleMethod( "scene.load", "Load a scene", "sceneName", "mode" )]
+		public static void LoadScene( string sceneName, string mode )
 		{
-			if (SceneManager.GetSceneByName(sceneName).IsValid())
-			{ Debug.Log($"Scene {sceneName} already loaded"); return; }
-			var parsedMode = (LoadSceneMode)Enum.Parse(typeof(LoadSceneMode), mode, true);
-			SceneManager.LoadSceneAsync(sceneName, parsedMode);
+			if ( SceneManager.GetSceneByName( sceneName ).IsValid() )
+			{
+				Debug.Log( $"Scene {sceneName} already loaded" );
+				return;
+			}
+			var parsedMode = ( LoadSceneMode )Enum.Parse( typeof(LoadSceneMode), mode, true );
+			SceneManager.LoadSceneAsync( sceneName, parsedMode );
 		}
 
-		[ConsoleMethod("scene.unload", "Unload a scene", "sceneName")]
-		public static void UnloadScene(string sceneName) => SceneManager.UnloadSceneAsync(sceneName);
+		[ConsoleMethod( "scene.unload", "Unload a scene", "sceneName" )]
+		public static void UnloadScene( string sceneName ) =>
+			SceneManager.UnloadSceneAsync( sceneName );
 	}
 }
