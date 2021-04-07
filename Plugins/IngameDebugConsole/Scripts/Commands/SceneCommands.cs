3f99c5048b3445c213679f06c41a34cdcbc6cf70
tonygiang <tony_s_giang@yahoo.com>
2021-04-07 21:44:43 +0700

    Add SceneCommands.

---
+++ Plugins/IngameDebugConsole/Scripts/Commands/SceneCommands.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+
+namespace IngameDebugConsole.Commands
+{
+	public class SceneCommands
+	{
+		[ConsoleMethod("scene.load", "Load a scene", "sceneName", "mode")]
+		public static void LoadScene(string sceneName, string mode)
+		{
+			if (SceneManager.GetSceneByName(sceneName).IsValid())
+			{ Debug.Log($"Scene {sceneName} already loaded"); return; }
+			var parsedMode = (LoadSceneMode)Enum.Parse(typeof(LoadSceneMode), mode, true);
+			SceneManager.LoadSceneAsync(sceneName, parsedMode);
+		}
+
+		[ConsoleMethod("scene.unload", "Unload a scene", "sceneName")]
+		public static void UnloadScene(string sceneName) => SceneManager.UnloadSceneAsync(sceneName);
+	}
+}
