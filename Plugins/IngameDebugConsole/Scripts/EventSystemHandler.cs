d0bb5ed40d5dbe2574db7f4edab8c9f784f1df5a
yasirkula <yasirkula@yahoo.com>
2022-07-25 13:31:12 +0300

    The embedded EventSystem is activated only when needed to avoid duplicate EventSystem warnings in the Console

---
+++ Plugins/IngameDebugConsole/Scripts/EventSystemHandler.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+namespace IngameDebugConsole
+{
+	// Avoid multiple EventSystems in the scene by activating the embedded EventSystem only if one doesn't already exist in the scene
+	[DefaultExecutionOrder( 1000 )]
+	public class EventSystemHandler : MonoBehaviour
+	{
+#pragma warning disable 0649
+		[SerializeField]
+		private GameObject embeddedEventSystem;
+#pragma warning restore 0649
+
+		private void OnEnable()
+		{
+			SceneManager.sceneLoaded -= OnSceneLoaded;
+			SceneManager.sceneLoaded += OnSceneLoaded;
+			SceneManager.sceneUnloaded -= OnSceneUnloaded;
+			SceneManager.sceneUnloaded += OnSceneUnloaded;
+
+			ActivateEventSystemIfNeeded();
+		}
+
+		private void OnDisable()
+		{
+			SceneManager.sceneLoaded -= OnSceneLoaded;
+			SceneManager.sceneUnloaded -= OnSceneUnloaded;
+
+			DeactivateEventSystem();
+		}
+
+		private void OnSceneLoaded( Scene scene, LoadSceneMode mode )
+		{
+			ActivateEventSystemIfNeeded();
+		}
+
+		private void OnSceneUnloaded( Scene current )
+		{
+			// Deactivate the embedded EventSystem before changing scenes because the new scene might have its own EventSystem
+			DeactivateEventSystem();
+		}
+
+		private void ActivateEventSystemIfNeeded()
+		{
+			if( embeddedEventSystem && !EventSystem.current )
+				embeddedEventSystem.SetActive( true );
+		}
+
+		private void DeactivateEventSystem()
+		{
+			if( embeddedEventSystem )
+				embeddedEventSystem.SetActive( false );
+		}
+	}
+}
